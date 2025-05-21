db2d56effc0807edb8122e447193d324f49467f9
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2025-05-22 00:12:27 +0800

    add login form  and its style

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,42 @@
+import { useState } from "react";
+import styles from "../styles/LoginForm.module.css";
+
 const LoginForm = () => {
-    return (
-        <p>This is login form</p>
-    )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const editUsername = (e) => {
+    const newValue = e.target.value;
+    setUsername(newValue);
+  }
+
+  const editPassword = (e) => {
+    const newValue = e.target.value;
+    setPassword(newValue);
+  }
+
+  return (
+    <div className={styles.base}>
+      <form className={styles.form}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={username}
+          onChange={editUsername}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={editPassword}
+        />
+        <button className={styles.loginBtn}>LOG-IN</button>
+        <p className={styles.signUpText}>Don't have an account? <a href="/">Sign up</a></p>
+      </form>
+    </div>
+  );
+};
 
 export default LoginForm;
