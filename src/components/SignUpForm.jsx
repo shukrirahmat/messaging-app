327d70ff625a8d3be5e81a46ff09d88e9878facc
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2025-05-24 08:15:11 +0800

    add sign up request

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "../styles/SignUpForm.module.css";
+import fetchURL from "../fetchURL.js";
 
 const signUpForm = () => {
+  const navigate = useNavigate();
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
   const [usernameErr, setUsernameErr] = useState(null);
   const [passwordErr, setPasswordErr] = useState(null);
   const [passwordErr2, setPasswordErr2] = useState(null);
+  const [isSigningUp, setIsSigningUp] = useState(false);
+  const [signUpErr, setSignUpErr] = useState("");
 
   const isFirstCharALetter = (string) => {
     return isNaN(string.charAt(0)) && string.charAt(0) !== "_";
@@ -34,7 +40,7 @@ const signUpForm = () => {
       setPasswordErr("*Password should have 6 characters minimum");
     } else {
       setPasswordErr("");
-      setPasswordErr2("")
+      setPasswordErr2("");
     }
   };
 
@@ -48,11 +54,64 @@ const signUpForm = () => {
     }
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSignUpErr("");
+
+    if (username.length < 1 || password.length < 1 || password2.length < 1) {
+      if (username.length < 1) {
+        setUsernameErr("*Username is required");
+      }
+      if (password.length < 1) {
+        setPasswordErr("*Password is required");
+      }
+      if (password2.length < 1) {
+        setPasswordErr2("*Need to re-enter password");
+      }
+    } else if (
+      !username.match("^[a-zA-Z0-9_]+$") ||
+      !isFirstCharALetter(username) ||
+      password !== password2 ||
+      password.length < 6
+    ) {
+      // DO NOTHING
+    } else {
+      setIsSigningUp(true);
+
+      fetch(fetchURL + "/user", {
+        mode: "cors",
+        method: "POST",
+        headers: { "Content-type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams({
+          username,
+          password,
+        }),
+      })
+        .then((response) => {
+          if (response.ok) return response.json();
+          else throw new Error("Failed to sign up. server error");
+        })
+        .then((data) => {
+          setIsSigningUp(false);
+          if (!data.success) {
+            setUsernameErr(data.message);
+          } else {
+            navigate("/");
+            // Change this to automatically log in later!
+          }
+        })
+        .catch((err) => {
+          setSignUpErr(err.message);
+          setIsSigningUp(false);
+        });
+    }
+  };
+
   return (
     <div className={styles.base}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div>
-            <label htmlFor="username">USERNAME</label>
+          <label htmlFor="username">USERNAME</label>
           <input
             type="text"
             name="username"
@@ -64,7 +123,7 @@ const signUpForm = () => {
           {usernameErr && <p className={styles.errorMsg}>{usernameErr}</p>}
         </div>
         <div>
-            <label htmlFor="password">PASSWORD</label>
+          <label htmlFor="password">PASSWORD</label>
           <input
             type="password"
             name="password"
@@ -76,7 +135,7 @@ const signUpForm = () => {
           {passwordErr && <p className={styles.errorMsg}>{passwordErr}</p>}
         </div>
         <div>
-            <label htmlFor="password2">CONFIRM PASSWORD</label>
+          <label htmlFor="password2">CONFIRM PASSWORD</label>
           <input
             type="password"
             name="password2"
@@ -86,7 +145,10 @@ const signUpForm = () => {
           />
           {passwordErr2 && <p className={styles.errorMsg}>{passwordErr2}</p>}
         </div>
-        <button className={styles.signUpBtn}>SIGN UP</button>
+        <button className={styles.signUpBtn} disabled={isSigningUp}>
+          {isSigningUp ? "SIGNING UP..." : "SIGN UP"}
+        </button>
+        {signUpErr && <p className={styles.signUpErr}>{signUpErr}</p>}
       </form>
     </div>
   );
---
+++ src/fetchURL.js
@@ -0,0 +1,3 @@
+const url = "https://rural-myrah-shkrrhmt-58679d76.koyeb.app";
+
+export default url;
